21205798c5a94cbddb86a8b7eed500f08a6d0b4a
henriquesdias <henrique.dias1915@gmail.com>
2023-03-26 15:10:01 -0300

    feat: creates a router to insert informations about person into database

--- back-end/src/controllers/informationsController.ts
+++ back-end/src/controllers/informationsController.ts
@@ -3,12 +3,19 @@ import { Request, Response } from "express";
 import db from "../db/postgres";
 
 export default async function createInformations(req: Request, res: Response) {
+  const { name, linkedinUrl, githubUrl } = req.body;
+  if (!name || !linkedinUrl || !githubUrl) {
+    return res.sendStatus(422);
+  }
   try {
-    const data = await db.query("SELECT * FROM informations");
-    console.log(data);
+    const data = await db.query(
+      "INSERT INTO informations (name,linkedin_url,github_url) VALUES ($1,$2,$3)",
+      [name, linkedinUrl, githubUrl]
+    );
 
-    res.send("hello");
+    res.status(201).send({ name, linkedinUrl, githubUrl });
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
 }
